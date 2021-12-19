25fb805a39c7d0cbdbb2c1d25c6bb59e31d030b4
Nato Welch <nato+git@6ax.org>
2021-12-18 18:50:21 -0800

    tests//PullTest.php: tests for shell pipe plumbing, a extant/intent merging behavior.

--- tests/php/pest/PullTest.php
+++ tests/php/pest/PullTest.php
@@ -5,16 +5,94 @@ namespace SixAcross\Confix\Tests;
 use Symfony\Component\Yaml\Yaml;
 
 
-it('Pulls the values for person #1 from the api. ', function() {
+$exec_for_outputs = function( string $command ) {
 
-    exec( 
-        __DIR__ .'/../../../bin/pull.php '. __DIR__ .'/../../examples/person1.yml', 
-        $output_lines, 
-        $exit_code 
-      );
-    expect($exit_code)->toBeEmpty();
-    $output = Yaml::parse( implode( "\n", $output_lines ) );
+        $process = proc_open(
+            $command,
+            [ [ 'pipe', 'r' ], [ 'pipe', 'w' ], [ 'pipe', 'w' ] ],
+            $pipes
+          );
+        expect($process)->toBeResource();
+        
+        $stderr = stream_get_contents($pipes[2]);
+        $stdout = stream_get_contents($pipes[1]);
+        
+        fclose($pipes[0]);
+        fclose($pipes[1]);
+        fclose($pipes[2]);
+        $exit_code = proc_close($process);
+        
+        return [ $stdout, $stderr, $exit_code ];
+};
+
+
+it( 'accepts intent input on stdin when passed a dash as the input file argument. ', 
+    function() use ( $exec_for_outputs ) {
+        [ $stdout, $stderr, $exit_code ] = $exec_for_outputs( 
+            'cat '. __DIR__ .'/../../examples/person1.intent.yml | '. __DIR__ .'/../../../bin/pull.php - '
+          );
+        
+        expect($exit_code )->toBeEmpty();
+    }
+  );
     
-    $this->assertMatchesYamlSnapshot($output);
-    
-});
+it( 'produces intent output on stdout when passed a dash as the input file argument. ', 
+    function() use ( $exec_for_outputs ) {
+        [ $stdout, $stderr, $exit_code ] = $exec_for_outputs( 
+            'cat '. __DIR__ .'/../../examples/person1.intent.yml | '. __DIR__ .'/../../../bin/pull.php - '
+          );
+        
+        expect($exit_code )->toBeEmpty();
+        expect($stderr    )->toBeEmpty();
+        $this->assertMatchesYamlSnapshot( Yaml::parse($stdout) );
+    }
+  );
+  
+it( 'produces non-intent output on stderr (only) when passed a dash as the input file argument. ',
+    function() use ( $exec_for_outputs ) {
+        [ $stdout, $stderr, $exit_code ] = $exec_for_outputs( 
+            'echo "non-yaml garbage" | '. __DIR__ .'/../../../bin/pull.php - '
+          );
+        
+        expect($exit_code )->toBe(255);
+        expect($stdout    )->toBe('');
+        expect($stderr    )->not()->toBeEmpty();
+    }
+  );
+
+
+it( 'writes extant values to intent resources where those keys already appear in intent. ', 
+    function() use ( $exec_for_outputs ) {
+        [ $stdout, $stderr, $exit_code ] = $exec_for_outputs( 
+            'cat '. __DIR__ .'/../../examples/person1.intent.yml | '. __DIR__ .'/../../../bin/pull.php - '
+          );
+        expect($exit_code)->toBe(0);
+        expect($stderr)->toBe('');
+        $this->assertMatchesYamlSnapshot( Yaml::parse($stdout) );
+    }
+  );
+  
+it( 'does not write extant values to intent resources where those resources or keys do not appear in intent. ',
+    function() use ( $exec_for_outputs ) {
+        [ $stdout, $stderr, $exit_code ] = $exec_for_outputs( 
+            'cat '. __DIR__ .'/../../examples/person1.nocontent.intent.yml | '. __DIR__ .'/../../../bin/pull.php - '
+          );
+        expect($exit_code)->toBe(0);
+        expect($stderr)->toBe('');
+        $this->assertMatchesYamlSnapshot( Yaml::parse($stdout) );
+    }
+  );
+  
+it( 'writes extant values to intent for keys not appearing in intent when the --all-values option is passed. ', 
+    function() use ( $exec_for_outputs ) {
+        [ $stdout, $stderr, $exit_code ] = $exec_for_outputs( 
+            'cat '. __DIR__ .'/../../examples/person1.nocontent.intent.yml | '
+                . __DIR__ .'/../../../bin/pull.php --all-values - '
+          );
+        expect($exit_code)->toBe(0);
+        expect($stderr)->toBe('');
+        $this->assertMatchesYamlSnapshot( Yaml::parse($stdout) );
+    }
+  );
+
+
