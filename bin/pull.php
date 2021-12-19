b42627510ea2d1576ec7de0c49ef3cb3d0198af9
Nato Welch <nato+git@6ax.org>
2021-12-18 18:47:26 -0800

    bin/pull.php: make use of a recursive closure to merge extant values into intent.

--- bin/pull.php
+++ bin/pull.php
@@ -20,6 +20,26 @@ $confix_file = array_shift($args);
     $confix = Yaml::parse( file_get_contents( $confix_file ) );
     $resources = $confix['resources'];
 
+    $recurse = function( array $extant_array, array &$intent_array ) use ( &$recurse, $options ) {
+
+        $keys = array_keys( $intent_array );
+        
+        foreach( $keys as $key ) {
+          
+            if ( is_array($extant_array[$key] ?? null) and 
+                (   
+                    is_array($intent_array[$key]) 
+                  )
+              ) {
+                return $recurse( $extant_array[$key], $intent_array[$key] );
+                
+            } elseif ( array_key_exists( $key, $extant_array ) ) {
+                $intent_array[$key] = $extant_array[$key] ?? null;
+            }
+            
+        }
+    };
+
     foreach ( $resources as $resource_index => $resource ) {
       
         $response = Requests::get( $resource['uri'] );
@@ -30,8 +50,6 @@ $confix_file = array_shift($args);
             512,
             JSON_THROW_ON_ERROR
           );
-
-        $confix['resources'][$resource_index]['content'] = $content;
         
         $extant = [
             'status'  => $response->status_code,
@@ -39,6 +57,7 @@ $confix_file = array_shift($args);
             'headers' => $response->headers->getAll(),
           ];
         
+        $recurse( $extant, $confix['resources'][$resource_index] );
         
     }
     
